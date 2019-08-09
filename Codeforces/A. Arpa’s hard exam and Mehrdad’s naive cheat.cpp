b8d5335ee5671c37df9b4cdb92d4e0dc00df72f9
Sharif-Al-Hosafy <52129239+Sharif-Al-Hosafy@users.noreply.github.com>
2019-08-09 14:09:35 +0200

    Add files via upload

---
+++ Codeforces/A. Arpa’s hard exam and Mehrdad’s naive cheat.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+int n;
+cin>>n;
+
+if(n == 0) cout<<"1";
+else
+{
+    int arr[] = {8, 4, 2, 6};
+    cout<< arr[ (n-1) % 4 ];
+}
+    return 0;
+}
