58a5731f30d871df564937f6793053d36198dcca
Sharif-Al-Hosafy <52129239+Sharif-Al-Hosafy@users.noreply.github.com>
2020-06-02 22:58:51 +0200

    Add files via upload

---
+++ leetcode/Symmetric Tree.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var isSymmetric = function (root) {
+  if (!root) return true;
+
+  function isMirror(s, t) {
+    if (!s && !t) {
+      return true;
+    }
+    if (!s || !t || s.val !== t.val) {
+      return false;
+    }
+    return isMirror(s.left, t.right) && isMirror(s.right, t.left);
+  }
+
+  return isMirror(root.left, root.right);
+};
