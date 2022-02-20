55e22f11db4e3e7395aecdc1f4563815fc63f2bb
Sharif_Hosafy <Sherifismail44@gmail.com>
2022-02-21 01:28:25 +0200

    codeforces traininnng

---
+++ Codeforces/PetyaAndStrings.js
@@ -0,0 +1,7 @@
+var str1 = readline();
+
+var str2 = readline();
+
+if (str1.toLowerCase() < str2.toLowerCase()) print(-1);
+else if (str1.toLowerCase() > str2.toLowerCase()) print(1);
+else print(0);
